9118092016bfef46074be41300b3fc74dd391752
John Holiver <johnholiver@gmail.com>
2016-04-29 15:30:54 +0200

    Initial maven project
    Introduces also TakMain as an Hello World implementation

---
+++ src/main/java/johnholiver/game/TakMain.java
@@ -0,0 +1,13 @@
+package johnholiver.game;
+
+/**
+ * Hello world!
+ *
+ */
+public class TakMain {
+
+	public static void main(String[] args)
+	{
+        System.out.println( "Hello World!" );
+    }
+}
